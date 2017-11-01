bd63fc3d61c6ee07a05bed3d71fb7db666aa7aee
Alexander Heimbuch <github@heimbu.ch>
2017-11-01 16:19:38 +0100

    Prebundle Package

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,13 +1,20 @@
+const path = require('path')
+
 const { createConfig, entryPoint, setOutput } = require('@webpack-blocks/webpack2')
 const babel = require('@webpack-blocks/babel6')
 
 module.exports = createConfig([
   entryPoint({
-    bundle: './src/index.js',
-    actions: './src/actions.js',
-    props: './src/props.js',
-    events: './src/events.js'
+    index: './index.js',
+    actions: './actions.js',
+    audio: './audio.js',
+    events: './events.js',
+    props: './props.js'
   }),
   babel(),
-  setOutput('./[name].js')
+  setOutput({
+    libraryTarget: 'commonjs',
+    filename: '[name].js',
+    path: path.resolve('./dist')
+  })
 ])

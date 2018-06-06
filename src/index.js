47d6095b9a08090843ea3fea675dae44289aa3e3
Alexander Heimbuch <github@heimbu.ch>
2018-06-06 19:56:22 +0200

    fix(utils): Removes unused export

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import { video } from './video'
 import { events } from './events'
 import { actions } from './actions'
 import { props } from './props'
-import { utils } from './utils'
 
 export {
   audio,
   video,
   events,
   props,
-  actions,
-  utils
+  actions
 }

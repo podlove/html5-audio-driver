772d3c027023fbcac7e30e70bf8d6bde1bf07460
Alexander Heimbuch <github@heimbu.ch>
2018-06-30 17:57:38 +0200

    fix(audio): Exports audo factory

--- src/audio.js
+++ src/audio.js
@@ -3,4 +3,4 @@ import { compose } from 'ramda'
 import { createSourceNodes, mediaNode } from './media'
 import { mountNode, toArray } from './utils'
 
-export const video = sources => compose(mountNode, createSourceNodes(mediaNode('audio')), toArray)(sources)
+export const audio = sources => compose(mountNode, createSourceNodes(mediaNode('audio')), toArray)(sources)
